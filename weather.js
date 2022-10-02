1cd92c32467322332962456194b384cd9b3b929e
lucidwrld <kabiawubenedict@gmail.com>
2022-10-02 16:11:23 +0100

    weather app

---
+++ weather.js
@@ -0,0 +1,43 @@
+let weather = {
+ "apiKey": "f9b90ebbb52d8820b8681879ed3921ad",
+ fetchWeather: function(city){
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city 
+  + "&units=metric&appid=" 
+  + this.apiKey)
+
+  
+  .then((response) => response.json())
+  .then((data) => this.displayWeather(data))
+ },
+ displayWeather: function(data){
+  const {name} = data
+  const { icon, description} = data.weather[0]
+  const { temp, humidity} = data.main
+  const { speed} = data.wind
+  console.log(name,icon,description,temp,humidity,speed)
+  document.querySelector(".city").innerHTML = `Weather in ${name}`
+  document.querySelector(".temp").innerHTML = `${temp}°C`
+  document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`
+  document.querySelector(".description").innerHTML = `${description}`
+  document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`
+  document.querySelector(".wind").innerHTML = `Wind speed: ${speed} km/h`
+  document.querySelector(".forecast").classList.remove("loading")
+ },
+ search: function(){
+  this.fetchWeather(document.querySelector(".search-bar").value)
+ }
+}
+
+document.querySelector(".card button").addEventListener("click", function() {
+weather.search()
+})
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+if(event.key == "Enter"){
+ weather.search()
+}
+ })
+ 
+
+
+
+
